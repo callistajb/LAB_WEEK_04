8d52c70381d2ac4539d270e90df57f5391955ae5
callistajb <callistabandono71@sma.belajar.id>
2025-09-22 12:48:01 +0700

    Commit No. 4 - Assignment

--- app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
+++ app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.lab_week_04.R
+import android.content.Context
 
 val TABS_FIXED = listOf(
     R.string.starbucks_title,
@@ -13,12 +14,18 @@ val TABS_FIXED = listOf(
     R.string.kopikenangan_title,
 )
 
-class CafeAdapter (fragmentManager: FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(fragmentManager, lifecycle) {
+class CafeAdapter (fragmentManager: FragmentManager, lifecycle: Lifecycle, private val context: Context) : FragmentStateAdapter(fragmentManager, lifecycle) {
     override fun getItemCount(): Int {
         return TABS_FIXED.size
     }
 
     override fun createFragment(position: Int): Fragment {
-        return CafeDetailFragment() // Make sure CafeDetailFragment is also in the correct package or imported correctly.
+        val content = when (position) {
+            0 -> R.string.starbucks_desc
+            1 -> R.string.janjijiwa_desc
+            2 -> R.string.kopikenangan_desc
+            else -> 0
+        }
+        return CafeDetailFragment.newInstance(context.getString(content))
     }
 }
--- app/src/main/java/com/example/lab_week_04/CafeDetailFragment.kt
+++ app/src/main/java/com/example/lab_week_04/CafeDetailFragment.kt
@@ -6,11 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.lab_week_04.R
+import android.widget.TextView
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+private const val TAB_CONTENT = "TAB_CONTENT"
 
 /**
  * A simple [Fragment] subclass.
@@ -21,10 +24,12 @@ class CafeDetailFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var content: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
+            content = it.getString(TAB_CONTENT)
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
@@ -38,7 +43,13 @@ class CafeDetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_cafe_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.content_description)?.text = content
+    }
+
     companion object {
+        private const val TAB_CONTENT = "TAB_CONTENT"
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
@@ -49,6 +60,12 @@ class CafeDetailFragment : Fragment() {
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
+        fun newInstance(content: String) =
+            CafeDetailFragment().apply {
+                arguments = Bundle().apply {
+                    putString(TAB_CONTENT, content)
+                }
+            }
         fun newInstance(param1: String, param2: String) =
             CafeDetailFragment().apply {
                 arguments = Bundle().apply {
